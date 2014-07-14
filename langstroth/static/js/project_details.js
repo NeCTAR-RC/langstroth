665f579e4993bb14230de5dad86cd2015b28cdda
Richard Rothwell <rrothwell@v3.org.au>
2014-07-14 16:31:00 +1000

    RR: minor tidy up of Javascript code.

--- langstroth/static/js/project_details.js
+++ langstroth/static/js/project_details.js
@@ -26,25 +26,27 @@ var toolTip = d3.select("body")
 				.text("a simple tooltip");
 
 
+//==== HTML Table For Project Details.
+// Popluated on page load. Dynamic update afger page load is not supported.
+
 function tabulateSummary(pageAreaSelector, project_summary) {
 
+	// Define the table with heading.
 	var table = d3.select(pageAreaSelector).append("table")
-					.attr("class", "details-table");
-	
-	var caption = table.append("caption")
-		.text("Aggregate Allocation"); 
-	thead = table.append("thead"), // Not used.
+					.attr("class", "details-table");	
+	table.append("caption").text("Aggregate Allocation"); 
 	tbody = table.append("tbody"); 
 
+	// Add rows with rows (tr), row headings (th) and the detail entries (td).
 	var rows = tbody.selectAll("tr")
-		.data(function(row) { 
-			var keys = [];
-			for (key in headings) {
-				keys.push(key);
-			} 
-			return keys; })
-		.enter()
-		.append("tr");
+					.data(function(row) { 
+						var keys = [];
+						for (key in headings) {
+							keys.push(key);
+						} 
+						return keys; })
+					.enter()
+					.append("tr");
 	
 	rows.append("th")
 		.text(function(row) { 
@@ -56,129 +58,136 @@ function tabulateSummary(pageAreaSelector, project_summary) {
 			return project_summary[row]; 
 		});
 }
+
+//==== Pie Chart for Instance and Core Quota.
+
+// Pie chart constants.
+
+var PIE_CHART_WIDTH = 100;
+var PIE_CHART_HEIGHT = 100;
+var PIE_CHART_RADIUS = Math.min(PIE_CHART_WIDTH, PIE_CHART_HEIGHT) / 2;
+
 function graphQuota(pageAreaSelector, quotaKey, usage) {
 		
-	var width = 100,
-	    height = 100,
-	    radius = Math.min(width, height) / 2;
-
 	var color = d3.scale.ordinal()
-		// used then unused.
-	    .range(["#5687d1", "#dddddd"]);
+				// Color for used quota then unused quota.
+			    .range(["#5687d1", "#dddddd"]);
 
 	var pie = d3.layout.pie()
-	    .value(function(d) { 
-	    	return d; 
-	    });
+				.value(function(d) { 
+						return d; 
+				});
 
 	var arc = d3.svg.arc()
-	    .outerRadius(radius)
-	    .innerRadius(radius*0.5)
-	    .startAngle(function(d) { return (2*Math.PI) -d.startAngle; })
-	    .endAngle(function(d) { return (2*Math.PI) -d.endAngle; });
+				.outerRadius(PIE_CHART_RADIUS)
+				.innerRadius(PIE_CHART_RADIUS*0.5)
+				.startAngle(function(d) { return 2*Math.PI - d.startAngle; })
+				.endAngle(function(d) { return 2*Math.PI - d.endAngle; });
 
 	var svg = d3.select(pageAreaSelector).append("svg")
-	    .attr("width", width)
-	    .attr("height", height)
-	  .append("g")
-	    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+				.attr("PIE_CHART_WIDTH", PIE_CHART_WIDTH)
+				.attr("PIE_CHART_HEIGHT", PIE_CHART_HEIGHT)
+				.append("g")
+					.attr("transform", "translate(" + PIE_CHART_WIDTH / 2 + "," + PIE_CHART_HEIGHT / 2 + ")");
 
-		var g = svg.selectAll(".arc")
-		      .data(pie(usage))
-		    .enter().append("g")
-		      .attr("class", "arc");
+	var g = svg.selectAll(".arc")
+				.data(pie(usage))
+				.enter().append("g")
+				.attr("class", "arc");
 		
-		  g.append("path")
-		      .attr("d", arc)
-		      .style("fill", function(d, i) { 
-		    	  return color(i); 
-		    	 });
+	g.append("path")
+				.attr("d", arc)
+				.style("fill", function(d, i) { 
+					return color(i); 
+				});
 
-		  g.append("text")
-		      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-		      .attr("dy", ".35em")
-		      .style("text-anchor", "middle")
-		      .text(function(d) { 
-		    		var usageQuota = (usage[0] + usage[1]) + "";
-		    	  return d.data == "0" ? "" : d.data + "/" + usageQuota; 
-		    	 });
+	g.append("text")
+				.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+				.attr("dy", ".35em")
+				.style("text-anchor", "middle")
+				.text(function(d) { 
+					var usageQuota = (usage[0] + usage[1]) + "";
+					return d.data == "0" ? "" : d.data + "/" + usageQuota; 
+				});
 }
 
+//==== Pie Chart to show Field of Research Partitioning for Project.
+
 function graphFieldOfResearch(pageAreaSelector, forUsage, forTranslation) {
-	var width = 100,
-    	height = 100,
-	    radius = Math.min(width, height) / 2;
 
 	var color = d3.scale.ordinal()
-		// for 1, 2 3.
-	    .range(["#ff0000", "#00ff00", "#0000ff"]);
+				// for first, second and third FOR's.
+				.range(["#ff0000", "#00ff00", "#0000ff"]);
 
 	var pie = d3.layout.pie()
-	    .value(function(d) { 
-	    	return d.percent; 
-	    });
+				.value(function(d) { 
+					return d.percent; 
+				});
 
 	var arc = d3.svg.arc()
-	    .outerRadius(radius)
-	    .innerRadius(radius*0.5)
-	    .startAngle(function(d) { return (2*Math.PI) -d.startAngle; })
-	    .endAngle(function(d) { return (2*Math.PI) -d.endAngle; });
+				.outerRadius(PIE_CHART_RADIUS)
+			    .innerRadius(PIE_CHART_RADIUS*0.5)
+			    .startAngle(function(d) { return 2*Math.PI - d.startAngle; })
+			    .endAngle(function(d) { return 2*Math.PI - d.endAngle; });
 
 	var svg = d3.select(pageAreaSelector).append("svg")
-	    .attr("width", width)
-	    .attr("height", height)
-	  .append("g")
-	    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+				.attr("PIE_CHART_WIDTH", PIE_CHART_WIDTH)
+				.attr("PIE_CHART_HEIGHT", PIE_CHART_HEIGHT)
+				.append("g")
+				.attr("transform", "translate(" + PIE_CHART_WIDTH / 2 + "," + PIE_CHART_HEIGHT / 2 + ")");
 
-	  var path = svg.datum(forUsage).selectAll("path")
-	      .data(pie)
-	    .enter().append("path")
-	      .attr("fill", function(d, i) { return color(i); })
-	      .attr("d", arc)
-	      .each(function(d) { 
-	    	  this._current = d; 
-	    	 }); // store the initial angles
+	var path = svg.datum(forUsage).selectAll("path")
+				.data(pie)
+				.enter().append("path")
+				.attr("fill", function(d, i) { return color(i); })
+				.attr("d", arc)
+				.each(function(d) { 
+					this._current = d; 
+				}); // store the initial angles
 
-	  var g = svg.selectAll(".arc")
-	      .data(pie(forUsage))
-	    .enter().append("g")
-	      .attr("class", "arc");
+	var g = svg.selectAll(".arc")
+				.data(pie(forUsage))
+				.enter().append("g")
+				.attr("class", "arc");
 
-	  var text = g.append("text")
-	      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-	      .attr("dy", ".35em")
-	      .style("text-anchor", "middle")
-	       .on("mouseover", showRelatedLabels)
-	       .on("mousemove", moveRelatedLabels)
-	       .on("mouseout", hideRelatedLabels)
-	      .text(function(d) { 
-	    	  return d.data.code; 
-	    	 });
+	var text = g.append("text")
+				.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+				.attr("dy", ".35em")
+				.style("text-anchor", "middle")
+				.on("mouseover", showRelatedLabels)
+				.on("mousemove", moveRelatedLabels)
+				.on("mouseout", hideRelatedLabels)
+				.text(function(d) { 
+					return d.data.code; 
+				});
 
 	//---- Popup showing details.
 		
-	  function showFor(d) {
-		  var markup = "<div class='for-container'>" 
-	  			+ forTranslation[d.data.code]
-	  			+ "</div>";
-		  toolTip.html(markup);
-	  }
+	function showFor(d) {
+		var markup = "<div class='for-container'>" 
+					+ forTranslation[d.data.code]
+					+ "</div>";
+		toolTip.html(markup);
+	}
 
-	  function showRelatedLabels(d, i) { 
-		  showFor(d);
-		  toolTip.style("visibility", "visible");
-	  }
+	function showRelatedLabels(d, i) { 
+		showFor(d);
+		toolTip.style("visibility", "visible");
+	}
 
-	  function moveRelatedLabels(d, i) { 
-	  	var top = (d3.event.pageY - 10) + "px";
-	  	var left = (d3.event.pageX + 10) + "px";
-	  	toolTip.style("top", top).style("left", left);
-	  }
+	function moveRelatedLabels(d, i) { 
+		var top = (d3.event.pageY - 10) + "px";
+		var left = (d3.event.pageX + 10) + "px";
+		toolTip.style("top", top).style("left", left);
+	}
 
-	  function hideRelatedLabels(d, i) { 
-	  	toolTip.style("visibility", "hidden");
-	  }
+	function hideRelatedLabels(d, i) { 
+		toolTip.style("visibility", "hidden");
+	}
 }
+
+//==== Project Allocation: Assembling the Pieces.
+// Table and 3 pie charts.
 
 function projectDetails() {
 	d3.json("/nacc/rest/for_codes", function(error, forTranslation) {
