91600c2dcb6f95f6efd4e1939072ca0eba26d66b
Russell Sim <russell.sim@gmail.com>
2014-09-29 09:36:41 +1000

    Reorganised project so the database will be initialised with reference data. Reverted change to langstroth/views.py Version 2. Change allocations.py to reduce variable usage. Logging improved. Some style improvements.
    Change-Id: I6f73a8f000fdaed8e746a94b7c29648d77a8e695

---
+++ nectar_allocations/tests/test_view.py
@@ -0,0 +1,48 @@
+from django.test import TestCase
+
+from nectar_allocations.models.forcode import ForCode
+
+class AllocationViewTest(TestCase):
+
+    fixtures = ['allocation_requests']
+    multi_db = True
+
+    # Web pages
+
+    def setUp(self):
+        ForCode.objects.create(code="1234", name="Biological necessity")
+        ForCode.objects.create(code="4321", name="Physical impossibility")
+
+    def test_page_index(self):
+        response = self.client.get("/allocations/")
+        self.assertEqual(200, response.status_code)
+
+    def test_page_visualisation(self):
+        response = self.client.get("/allocations/applications/approved/visualisation")
+        self.assertEqual(200, response.status_code)
+
+    def test_page_project(self):
+        response = self.client.get("/allocations/applications/1654/approved")
+        self.assertEqual(200, response.status_code)
+
+    def test_page_project_allocations(self):
+        response = self.client.get("/allocations/applications/1654/approved")
+        self.assertEqual(200, response.status_code)
+
+    # Web services with JSON pay loads.
+
+    def test_rest_allocation_tree(self):
+        response = self.client.get("/allocations/rest/applications/approved/tree")
+        self.assertEqual(200, response.status_code)
+
+    def test_rest_for_code(self):
+        response = self.client.get("/allocations/rest/for_codes")
+        self.assertEqual(200, response.status_code)
+
+    def test_rest_for_project_from_allocation_request_id(self):
+        response = self.client.get("/allocations/rest/applications/1654/approved")
+        self.assertEqual(200, response.status_code)
+
+    def test_rest_for_project_allocations_from_allocation_request_id(self):
+        response = self.client.get("/allocations/rest/applications/1654/history")
+        self.assertEqual(200, response.status_code)
---
+++ nectar_allocations/views.py
@@ -0,0 +1,66 @@
+from json import dumps
+from django.http import HttpResponse
+from django.shortcuts import render
+from nectar_allocations.models.forcode import ForCode
+from nectar_allocations.models.allocation import AllocationRequest
+
+
+# Web pages
+
+
+def index_page(request):
+    return allocation_visualisation_page(request)
+
+
+def allocation_visualisation_page(request):
+    context = {
+        "title": "Allocations",
+        "tagline": ""}
+    return render(request, "allocation_visualisation.html", context)
+
+
+def project_details_page(request, allocation_request_id):
+    allocation_dict = AllocationRequest \
+        .project_from_allocation_request_id(allocation_request_id)
+    context = {
+        "title": allocation_dict['project_name'].replace('_', ' '),
+        "tagline": "",
+        "allocation_request_id": allocation_request_id}
+    return render(request, "project_details.html", context)
+
+
+def project_allocations_page(request, allocation_request_id):
+    context = {
+        "title": "Project Allocations",
+        "tagline": "",
+        "allocation_request_id": allocation_request_id}
+    return render(request, "project_allocations.html", context)
+
+
+# Web services with JSON pay loads.
+
+
+def for_codes(request):
+    code_dict = ForCode.code_dict()
+    json_string = dumps(code_dict)
+    return HttpResponse(json_string, "application/json")
+
+
+def allocation_tree(request):
+    allocation_dict = AllocationRequest.restructure_allocations_tree()
+    json_string = dumps(allocation_dict)
+    return HttpResponse(json_string, "application/json")
+
+
+def project_summary(request, allocation_request_id):
+    allocation_dict = AllocationRequest \
+        .project_from_allocation_request_id(allocation_request_id)
+    json_string = dumps(allocation_dict)
+    return HttpResponse(json_string, "application/json")
+
+
+def project_allocations(request, allocation_request_id):
+    allocation_list = AllocationRequest \
+        .project_allocations_from_allocation_request_id(allocation_request_id)
+    json_string = dumps(allocation_list)
+    return HttpResponse(json_string, "application/json")
