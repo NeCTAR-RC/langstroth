3ae49ce2171e2ec5716eb0b48998d1d8721bc65b
Russell Sim <russell.sim@gmail.com>
2014-10-07 10:13:53 +1100

    Updated tox configuration moved to py.test
    Change-Id: I5561cea7b51db3627e70b5cf1f522127c5d739f5

--- run_tests.sh
+++ run_tests.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 set -e
 
-#Usage:
-# cd into the test directory then execute:
-# export DJANGO_TEST=True;../../run_tests.sh --settings=langstroth.settings
-
-DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-
-cd $DIR
-
-if [ -n "$*" ]; then
-    TESTS="$@"
-else
-    TESTS="langstroth nectar_allocations"
-fi
-
-./manage.py test --settings=langstroth.settings_test -v 2 $TESTS
+tox
