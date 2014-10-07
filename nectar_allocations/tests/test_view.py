869dd92874355baf28704e17707ea4910f98cf4f
Richard Rothwell <rrothwell@v3.org.au>
2014-10-07 15:38:49 +1100

    Reformat code to comply with  flake8 rules.
    Spacing, line lengths adjusted.
    Minimised #noqa annotaions.
    Adjusted .gitignore to ignore private data files.
    
    Change-Id: Idcb30df36af4a6f3f51edb75391b9ed271735671

--- nectar_allocations/tests/test_view.py
+++ nectar_allocations/tests/test_view.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 
 from nectar_allocations.models.forcode import ForCode
 
+
 class AllocationViewTest(TestCase):
 
     fixtures = ['allocation_requests']
@@ -18,21 +19,25 @@ class AllocationViewTest(TestCase):
         self.assertEqual(200, response.status_code)
 
     def test_page_visualisation(self):
-        response = self.client.get("/allocations/applications/approved/visualisation")
+        response = self.client.get(
+            "/allocations/applications/approved/visualisation")
         self.assertEqual(200, response.status_code)
 
     def test_page_project(self):
-        response = self.client.get("/allocations/applications/1654/approved")
+        response = self.client.get(
+            "/allocations/applications/1654/approved")
         self.assertEqual(200, response.status_code)
 
     def test_page_project_allocations(self):
-        response = self.client.get("/allocations/applications/1654/approved")
+        response = self.client.get(
+            "/allocations/applications/1654/approved")
         self.assertEqual(200, response.status_code)
 
     # Web services with JSON pay loads.
 
     def test_rest_allocation_tree(self):
-        response = self.client.get("/allocations/rest/applications/approved/tree")
+        response = self.client.get(
+            "/allocations/rest/applications/approved/tree")
         self.assertEqual(200, response.status_code)
 
     def test_rest_for_code(self):
@@ -40,9 +45,11 @@ class AllocationViewTest(TestCase):
         self.assertEqual(200, response.status_code)
 
     def test_rest_for_project_from_allocation_request_id(self):
-        response = self.client.get("/allocations/rest/applications/1654/approved")
+        response = self.client.get(
+            "/allocations/rest/applications/1654/approved")
         self.assertEqual(200, response.status_code)
 
     def test_rest_for_project_allocations_from_allocation_request_id(self):
-        response = self.client.get("/allocations/rest/applications/1654/history")
+        response = self.client.get(
+            "/allocations/rest/applications/1654/history")
         self.assertEqual(200, response.status_code)
