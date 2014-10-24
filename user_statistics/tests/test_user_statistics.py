f9330373ae59f6d2d6a1e283194765890812482f
Russell Sim <russell.sim@gmail.com>
2014-10-24 14:57:58 +1100

    Re-factored graphite querying into an object
    Change-Id: I6b06c4007c7971abc2f3dda1e4bc26bddc6bc32e

--- user_statistics/tests/test_user_statistics.py
+++ user_statistics/tests/test_user_statistics.py
@@ -1,14 +1,9 @@
-from django.test import TestCase
-from pytest import fail
-
-from requests import Response
-import requests
 from os import path
+
+import httpretty
 
 from user_statistics.services.user_statistics \
     import find_daily_accumulated_users
-from user_statistics.tests.expected_users_statistics \
-    import ExpectedUserStatistics
 
 
 def path_for_tests(file_name):
@@ -16,33 +11,35 @@ def path_for_tests(file_name):
     of the pathname and filename."""
     return path.abspath(path.join(path.dirname(__file__), file_name))
 
-
-def dummy_get(url, **kwargs):
-    if 'http://graphite.dev.rc.nectar.org.au/render' in url:
-        file_path = path_for_tests(
-            "./users_total_daily_graphite_response.json")
-        with open(file_path) as user_json_file:
-            user_json = user_json_file.read()
-        response = Response()
-        response.status_code = 200
-        response._content = user_json
-        return response
-
-    fail("No response for URL: '%s'" % url)
-    return Response()
+FILE_PATH = path_for_tests("./users_total_daily_graphite_response.json")
 
 
-class UserStatisticsTest(TestCase):
+@httpretty.activate
+def test_find_daily_accumulated_users():
+    httpretty.register_uri(
+        httpretty.GET,
+        'http://graphite.dev.rc.nectar.org.au/render/',
+        body=open(FILE_PATH).read(),
+        content_type="application/json")
 
-    def test_find_daily_accumulated_users_return_response(self):
-        saved_get = requests.get
-        requests.get = dummy_get
-        try:
-            actual_accumulated_users = \
-                find_daily_accumulated_users()
-            self.assertEqual(
-                ExpectedUserStatistics
-                .daily_accumulated_users,
-                actual_accumulated_users)
-        finally:
-            requests.get = saved_get
+    expected_daily_accumulated_users = [
+        {
+            "target": "Cumulative",
+            "datapoints": [
+                [0.0, 1324216800],
+                [0.0, 1324303200],
+                [2.0, 1325512800],
+                [3.0, 1325599200],
+            ]
+        },
+        {
+            "target": "Frequency",
+            "datapoints": [
+                [0.0, 1324303200],
+                [2.0, 1325512800],
+            ]
+        }
+    ]
+
+    actual_accumulated_users = find_daily_accumulated_users()
+    assert expected_daily_accumulated_users == actual_accumulated_users
