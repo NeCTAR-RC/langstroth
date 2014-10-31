5f80da5a721a99727347fc613be8374aa12b8bbf
Richard Rothwell <rrothwell@v3.org.au>
2014-10-31 13:29:37 +1100

    Using an extended path for the allocations URL will now navigate to the selected FOR level
    Housekeeping to keep the checkstyle stuff happy.
    
    Change-Id: Ieae25be0d33c89e5b2927c13591ed7cc1febf94b

--- langstroth/static/js/colour_palette.js
+++ langstroth/static/js/colour_palette.js
@@ -5,9 +5,13 @@
 // ColourPalette management class.
 
 function ColourPalette() {
-	var colorPalette = d3.scale.category20();
-	this.paletteStack = [colorPalette];
+  this.basePalette = d3.scale.category20();
+  this.paletteStack = [this.basePalette];
 }
+
+ColourPalette.prototype.reset = function () {
+  this.paletteStack = [this.basePalette];
+};
 
 //The palette that is relevant to this FOR tree level.
 
@@ -25,24 +29,24 @@ ColourPalette.prototype.getColour = function (colourIndex) {
 // and then uses that colour to generate a new palette.
 
 ColourPalette.prototype.push = function (colourIndex, datasetLength) {
-    var currentColour = this.getColour(colourIndex);
-    var newPalette = d3.scale.linear()
+  var currentColour = this.getColour(colourIndex);
+  var newPalette = d3.scale.linear()
           .domain([0, datasetLength + 10])
           .range([currentColour, "white"]);
-    this.paletteStack.push(newPalette);
+  this.paletteStack.push(newPalette);
 };
 
 // A previous palette to be used for the next outer level is exposed
 // on the top of the palette stack.
 
 ColourPalette.prototype.pop = function () {
-    // Restore next outer palette.
-	this.paletteStack.pop();
+  // Restore next outer palette.
+  this.paletteStack.pop();
 };
 
 // Go to target level.
 // home = level 1.
 // one in from home = level 2 etc..
 ColourPalette.prototype.popToLevel = function (targetLevel) {
-    this.paletteStack = this.paletteStack.slice(0, targetLevel);
+  this.paletteStack = this.paletteStack.slice(0, targetLevel);
 };
