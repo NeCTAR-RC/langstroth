e5820cf868cf87f616ee6c759609e632dc1bb7d4
Richard Rothwell <rrothwell@v3.org.au>
2014-10-31 13:29:37 +1100

    Using an extended path for the allocations URL will now navigate to the selected FOR level
    Housekeeping to keep the checkstyle stuff happy.
    More JSHint issues fixed.
    
    Change-Id: Ieae25be0d33c89e5b2927c13591ed7cc1febf94b

--- langstroth/static/js/allocations.js
+++ langstroth/static/js/allocations.js
@@ -10,7 +10,7 @@ function Allocations(allocationTree) {
 // The allocationObjects is the allocationTree object being passed in.
 Allocations.prototype.traverse = function(route) {
   var children = this.allocationTree;
-  var forCodes = route;
+  var forCodes = route.concat();
   return this.nextLevel(forCodes, children);
 };
 
@@ -87,4 +87,48 @@ Allocations.prototype.dataset = function(route, isCoreQuota) {
   var tree = route ? this.traverse(route) : this.allocationTree;
   return this.restructureAllocations(tree, isCoreQuota);
 };
+
+Allocations.prototype.assemblePath = function(forCodeComponents, codeCount) {
+	var forCodePath = [];
+	var isZeroPadding = false;
+	var forCode = forCodeComponents[0];
+	forCodePath.push(forCode);
+	for (var codeIndex = 1; codeIndex < codeCount; codeIndex++) {
+		forCode = forCodePath[codeIndex - 1];
+		var forCodeComponent = forCodeComponents[codeIndex];
+		isZeroPadding = isZeroPadding || forCodeComponent == "00";
+		if (!isZeroPadding) {
+			forCode += forCodeComponent;
+		}
+		forCodePath.push(forCode);
+	}
+	return forCodePath;
+};
+
+// FOR code parameter strings are interpreted as a 1,2 or 3 element path.
+// 00 components cause path extensions.
+// E.g. 053725 becomes /05/0537/053725
+// E.g. 050000 becomes /05/05/05
+// E.g. 053700 becomes /05/0537/0537
+// E.g. 0345 becomes /03/0345
+Allocations.prototype.parseForPath = function(pathExtension) {
+	var forCodePath = [];
+	var command = pathExtension.substring(1);
+	var FOR_COMMAND = "/FOR/";
+	if (command && command.indexOf(FOR_COMMAND) === 0) {
+		var forTarget = command.substring(FOR_COMMAND.length);
+		var codeCount = forTarget.length / 2;
+		if (codeCount == 1 || codeCount == 2 || codeCount == 3) {
+	    	var forCodeComponents = [];
+	    	for (var codeIndex = 0; codeIndex < codeCount; codeIndex++) {
+	    		var firstIndex = codeIndex * 2;
+	    		var forCodeComponent = forTarget.substr(firstIndex, 2);
+	    		forCodeComponents.push(forCodeComponent);
+	    	}
+			forCodePath = this.assemblePath(forCodeComponents, codeCount);
+		}
+	}
+	return forCodePath.reverse();
+};
+
 
