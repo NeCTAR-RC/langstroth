5fc525bb3286f49ad2f2ba51adfa3e71870aacf2
Richard Rothwell <rrothwell@v3.org.au>
2014-11-05 10:23:43 +1100

    Generate sitemap.xml to expose site to Google
    Disabled initial_data.json to fix tests on CI server.
    Extra line inserted in file to keep flake happy.
    Made view test slightly stronger.
    Add tests for sitemap class.
    Fixed bug in settings.py that was loading wrong database engine for Sites.
    
    Change-Id: I1d4fcd819b9792104abc8338ef1138ea99a788f1

---
+++ nectar_allocations/tests/test_sitemap.py
@@ -0,0 +1,37 @@
+from django.test import TestCase
+import mock
+from datetime import datetime
+
+from nectar_allocations.models.allocation import AllocationRequest
+from nectar_allocations.sitemap import AllocationsSitemap
+
+
+class SitemapTest(TestCase):
+
+    @mock.patch(
+        'nectar_allocations.models.allocation.AllocationRequest.'
+        'find_active_allocations')
+    def test_items(self, mock_find_active_allocations):
+        request0 = AllocationRequest(project_name="Project X", status="E")
+        request1 = AllocationRequest(project_name="Project Y", status="X")
+
+        mock_find_active_allocations.return_value = [request0, request1]
+        site_map = AllocationsSitemap()
+        actual_items = site_map.items()
+        self.assertEquals([request0, request0], actual_items)
+
+    def test_lastmod(self):
+        expected_datetime = datetime(2014, 10, 13)
+        request0 = AllocationRequest(project_name="Project X", status="E")
+        request0.modified_time = expected_datetime
+        site_map = AllocationsSitemap()
+        actual_modification_datetime = site_map.lastmod(request0)
+        self.assertEquals(expected_datetime, actual_modification_datetime)
+
+    def test_location(self):
+        request0 = AllocationRequest(project_name="Project X", status="E")
+        request0.id = 12345
+        site_map = AllocationsSitemap()
+        actual_location = site_map.location(request0)
+        expected_location = '/allocations/applications/12345/approved'
+        self.assertEquals(expected_location, actual_location)
