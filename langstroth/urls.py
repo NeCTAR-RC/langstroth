2711611f627a24e695f98722969131655f039a1d
Sam Morrison <sorrison@gmail.com>
2019-10-18 16:26:46 +1100

    Upgrade to django 1.11.11 (bionic version)
    Change-Id: I675270e8e0fd265e64ee89152c1c81947b9f8f77

--- langstroth/urls.py
+++ langstroth/urls.py
@@ -1,19 +1,19 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.views.generic.base import RedirectView
 
 from langstroth import error
+from langstroth import views
 
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
 
-    url(r'^$', 'langstroth.views.index', name='home'),
+    url(r'^$', views.index, name='home'),
 
     # Composition Visualisations
-    url(r'^composition/(?P<name>\w+)/$', 'langstroth.views.composition',
+    url(r'^composition/(?P<name>\w+)/$', views.composition,
         name='composition'),
-    url(r'^composition/(?P<name>\w+)/cores$',
-        'langstroth.views.composition_cores', name='composition'),
+    url(r'^composition/(?P<name>\w+)/cores$', views.composition_cores,
+        name='composition'),
 
     # Redirect from old url
     url(r'^domain/$', RedirectView.as_view(
@@ -21,15 +21,14 @@ urlpatterns = patterns(
         name='composition'),
 
     # Usage Visualisations
-    url(r'^growth/infrastructure/$', 'langstroth.views.growth', name='growth'),
+    url(r'^growth/infrastructure/$', views.growth, name='growth'),
     url(r'^growth/users/', include('user_statistics.urls')),
-    url(r'^growth/instance_count$', 'langstroth.views.total_instance_count'),
-    url(r'^growth/used_cores$', 'langstroth.views.total_used_cores'),
+    url(r'^growth/instance_count$', views.total_instance_count),
+    url(r'^growth/used_cores$', views.total_used_cores),
 
     # Allocations Browser
     url(r'^allocations/', include('nectar_allocations.urls')),
-
-)
+]
 
 handler500 = error.handler500
 handler400 = error.handler500
