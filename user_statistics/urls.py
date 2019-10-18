1b9152bcf0e8f8f69cac8bfa8bdd0b4488417c81
Sam Morrison <sorrison@gmail.com>
2019-10-18 16:26:46 +1100

    Upgrade to django 1.11.11 (bionic version)
    Change-Id: I675270e8e0fd265e64ee89152c1c81947b9f8f77

--- user_statistics/urls.py
+++ user_statistics/urls.py
@@ -1,12 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from user_statistics import views
 
 
-urlpatterns = patterns(
-    '',
-
+urlpatterns = [
     url(r'^$',
         views.user_registrations_page, name='user_registrations'),
     url(r'^rest/registrations/frequency$',
         views.registrations_frequency, name='registrations_frequency'),
-)
+]
