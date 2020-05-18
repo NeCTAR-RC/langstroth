47a61dc9186574b297ed403650b1f13e56640b44
Andy Botting <andy@andybotting.com>
2020-05-18 11:44:41 +1000

    Use pbr for parsing requirments
    Change-Id: If8b3fce6d4b83c7e38f87594595bd9c836482aff

--- setup.py
+++ setup.py
@@ -1,40 +1,37 @@
-from setuptools import setup, find_packages
-import uuid
+import setuptools
 
-try:
-    from pip._internal.req import parse_requirements
-except ImportError:
-    from pip.req import parse_requirements
+from pbr.packaging import parse_requirements
 
 
-session = uuid.uuid1()
-version = '0.1'
-requirements = parse_requirements("requirements.txt", session=session)
+setuptools.setup(
+    name='langstroth',
+    version='0.1',
+    description=('Status page for the NeCTAR Research Cloud'),
+    author='NeCTAR',
+    author_email='',
+    url='https://github.com/NeCTAR-RC/langstroth',
+    license='GPLv3',
+    packages=[
+        'langstroth',
+        'nectar_allocations',
+        'nectar_status',
+        'user_statistics',
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=parse_requirements(),
+    classifiers=(
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: '
+        + 'GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ),
+)
 
-
-setup(name='langstroth',
-      version=version,
-      description="Status page for the NeCTAR Research Cloud.",
-      long_description="""\
-""",
-      classifiers=[
-          'Development Status :: 2 - Pre-Alpha',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: '
-          + 'GNU General Public License v3 or later (GPLv3+)',
-          'Natural Language :: English',
-          "Programming Language :: Python :: 2",
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4'],
-      keywords='',
-      author='NeCTAR',
-      author_email='',
-      url='',
-      license='GPLv3',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[str(r.req) for r in requirements])
