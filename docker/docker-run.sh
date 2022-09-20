7c46ef7abef6f12b680710a6b5a0dcd75cfa4958
Sam Morrison <sorrison@gmail.com>
2022-09-20 18:51:57 +1000

    Attempt to load env secrets from vault if present
    Change-Id: I4024ac7979537615a9e4e59166537bb0958cdd14

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -e
 
+if [ -f /vault/secrets/settings ]; then
+    echo "** Loading secrets from /vault/secrets/settings **"
+    . /vault/secrets/settings
+else
+    echo "** Secrets not found! **"
+fi
+
 if [ "$DJANGO_MIGRATE:-no" == "yes" ]; then
   echo "** Starting Django migrate **"
   ./manage.py migrate
