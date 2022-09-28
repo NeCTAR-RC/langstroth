6ce85cce918c997514737316adc498e586fd481c
Sam Morrison <sorrison@gmail.com>
2022-09-28 17:32:37 +1000

    Update version to 2.0.0
    Change-Id: I284e974acb41e95b8053c3fefcf9faf7ecfc500d

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from pbr.packaging import parse_requirements
 
 setuptools.setup(
     name='langstroth',
-    version='0.1',
+    version='2.0.0',
     description=('Status page for the NeCTAR Research Cloud'),
     author='NeCTAR',
     author_email='',
