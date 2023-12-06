1fc0d4f5d7c94f6a631a4ca27d0475086af26c1b
Sam Morrison <sorrison@gmail.com>
2023-12-06 20:30:39 +1100

    use whitenoise storage backend for staticfiles
    Change-Id: I3d291e6ac91904cd2486b3c009638d120874a354

--- langstroth/defaults.py
+++ langstroth/defaults.py
@@ -73,6 +73,15 @@ DATABASES = {
     },
 }
 
+STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.files.storage.FileSystemStorage'
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 # The URL to your Nagios installation.
 NAGIOS_URL = "http://nagios.test/cgi-bin/nagios3/"
 
@@ -156,7 +165,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
