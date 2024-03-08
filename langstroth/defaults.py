123251dc0fde24cdc056a02f0cc632ebfbee345a
Sam Morrison <sorrison@gmail.com>
2024-03-08 11:05:56 +1100

    Change default language to en-au
    Change-Id: Ia367571cef943acf02ebd7cc3e900ef294fcf142

--- langstroth/defaults.py
+++ langstroth/defaults.py
@@ -143,7 +143,7 @@ TIME_ZONE = 'Australia/Melbourne'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-au'
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
